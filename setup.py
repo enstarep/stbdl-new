7752a5ee2a312b462c324c1ac64d8be0148e7318
The embodiment of disharmony <34@rncyk.org>
2026-03-12 04:17:37 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="stbl",
+    version="0.1.0",
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        "stbl": ["*.exe", "*.png"],
+    },
+    exclude_package_data={
+        "": ["*.rc"],
+    },
+    author="enstarep",
+    author_email="enstarep@rncyk.org",
+    description="A simple taskbar decoration library",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/theembodimentofdisharmony/stbl",
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Microsoft :: Windows"
+    ],
+    python_requires=">=3.0",
+)
